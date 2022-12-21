577710743aaccbdd81912a076b881849350a3dd1
JavierG2001 <Javier.Gonzalez42@alu.uclm.es>
2022-12-21 18:00:59 +0100

    pruebas con junit hechas y documentación (target) generada

--- Pr1/src/main/java/ISOFT2_B05/Pr1/Problema1.java
+++ Pr1/src/main/java/ISOFT2_B05/Pr1/Problema1.java
@@ -5,26 +5,40 @@ import java.util.GregorianCalendar;
 
 public class Problema1 {
     
-    private Calendar fecha;
+    private Calendar calendario;
+    
     public Problema1(int anio,int mes, int dia){
         
-        this.setFecha(anio,mes,dia);
+        this.setCalendario(anio,mes,dia);
                
     }
     
-    public void setFecha(int anio,int mes, int dia){
+    public Problema1() {
+    	
+    }
+    
+    
+    public void setCalendario(int anio,int mes, int dia){
         if(dia<0 || mes<0 || anio<0){
             throw new IllegalArgumentException("error en los argumentos, no se permiten nº negativos");
         }
-        this.fecha = new GregorianCalendar(anio,mes,dia);
+        this.calendario = new GregorianCalendar(anio,mes,dia);
+    }
+    
+    public void setFecha(int anio,int mes, int dia) {
+    	if(dia<0 || mes<0 || anio<0){
+            throw new IllegalArgumentException("error en los argumentos, no se permiten nº negativos");
+        }
+    	
+    	this.calendario.set(anio, mes, dia);
     }
     
     public boolean es_bisiesto(){
         boolean bisiesto=false;
         
         
-        int ultimasCifras = this.fecha.get(Calendar.YEAR) % 100;
-        int anio = this.fecha.get(Calendar.YEAR);
+        int ultimasCifras = this.calendario.get(Calendar.YEAR) % 100;
+        int anio = this.calendario.get(Calendar.YEAR);
         if(ultimasCifras % 4 == 0 && anio % 100 != 0)
             bisiesto = true;
         else if(ultimasCifras % 4 == 0 && anio % 100 == 0 && anio % 400 == 0)
--- Pr1/src/test/java/ISOFT2_B05/Pr1/Problema1Test.java
+++ Pr1/src/test/java/ISOFT2_B05/Pr1/Problema1Test.java
@@ -7,20 +7,57 @@ import org.junit.*;
 
 public class Problema1Test {
 	
-	@Before
-	public void Before() {
+	@Test
+	public void crearFecha() {
+		try {
+			Problema1 pObj = new Problema1(2000, 7, 8);
+		}catch(IllegalArgumentException e) {
+			fail("error al introducir los valores del año mes y día");
+		}
+	}
+	
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void crearFechaIncorrecta() {
+		Problema1 pObj = new Problema1(2000, -10, 16);
+	}
+	
+	
+	
+	@Test
+	public void actualizarFecha() {
+		Problema1 pObj = new Problema1(2000, 7, 8);
+		
+		try {
+			pObj.setFecha(2007, 9, 16);
+		}catch(IllegalArgumentException e) {
+			fail("error al introducir los valores del año mes y día");
+		}
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void actualizarFechaIncorrecta() {
+		Problema1 pObj = new Problema1(2000, 7, 8);
+	
+		pObj.setFecha(-2007, 9, -16);
 		
 	}
 	
 	
 	@Test
-	public void crearFecha() {
+	public void bisiesto() {
+		Problema1 pObj = new Problema1(2000, 7, 8);
+		boolean resultado1 = pObj.es_bisiesto();
 		
+		assertTrue(resultado1);
+		
+		
+		pObj.setFecha(2015, 5, 6);
+		
+		boolean resultado2 = pObj.es_bisiesto();
+		
+		assertFalse(resultado2);
 	}
 	
-	@After
-	public void After() {
-		
-	}
-
+	
 }
